184a1dc18cfb02221977a18899a4f23f4aaf3715
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-08-14 17:31:20 +0500

    Add files via upload

---
+++ q6.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Quaid-e-Azam";
+let message = "There are two powers in the world; one of the sword and other is the pen";
+console.log(famous_person, ":", message);
---
+++ q6.ts
@@ -0,0 +1,7 @@
+let famous_person: string = "Quaid-e-Azam";
+let message:string = "There are two powers in the world; one of the sword and other is the pen";
+
+console.log(famous_person,":",message);
+
+
+
